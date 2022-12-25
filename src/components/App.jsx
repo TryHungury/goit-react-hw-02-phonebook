35ae590de147332310b50b32027713d06761b33e
TryHungury <mukolanadebernuy@gmail.com>
2022-12-25 18:45:35 +0200

    update task phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { Component } from "react";
 import { Box } from "./box/Box";
 import { PhoneBook } from "./phonebook/PhoneBook";
 import { Contacts } from "./contacts/Contacts";
+import { nanoid } from "nanoid";
 
 const Title = styled.h1`
   border-radius: ${p=>p.theme.radii.normal};
@@ -29,21 +30,69 @@ const TitleH2 = styled.h2`
 export class App extends Component {
   state = {
     contacts: [],
+    filter: '',
     name: '',
+    number: ''
+  }
+
+  handleChange = (e) => {
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+
+    return this.setState({[inputName] : inputValue})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const name = e.target.elements.name.value;
+    const number = e.target.elements.number.value;
+
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    }
+
+    this.setState((prevState)=>{
+      return {
+        contacts: [...prevState.contacts, contact]
+      }
+    })
+    
+    this.state.name = "";
+    this.state.number = "";
+  }
+
+  handleDeleteContact = (id) => {
+    this.setState(prevState=>{
+      return {
+        contacts: prevState.contacts.filter((contact)=>contact.id !== id)
+      }
+    })
+  }
+
+  handleFilterContacts = (e) => {
+    const filterValue = e.target.value;
+    const filterName = e.target.name;
+
+    this.setState({[filterName]: filterValue})
   }
 
   render() {
-    const {contacts} = this.state;
+    const {name, number, contacts, filter} = this.state;
+    const visibleContacts = contacts.filter((contact) => contact.name.includes(filter))
+
     return (
       <Box height= "100%"  display= "flex" flexDirection="column" justifyContent= "space-evenly" alignItems= "center" fontSize= "40px" backgroundColor="backgroundSecondary">
         <Title>Ukraine Win❤️</Title>
-        <Box as={"section"}>
+        <Box display="flex" flexDirection="column" justifyContent= "space-evenly" alignItems= "center" as={"section"}>
           <TitleH2>Phonebook</TitleH2>
-          <PhoneBook></PhoneBook>
+          <PhoneBook name={name} number={number} onChange={this.handleChange} onSubmit={this.handleSubmit}></PhoneBook>
         </Box>
-        <Box as={"section"}>
+        <Box display="flex" flexDirection="column" justifyContent= "space-evenly" alignItems= "center" as={"section"}>
           <TitleH2>Contacts</TitleH2>
-          <Contacts contacts={contacts}></Contacts>
+          <Contacts contacts={visibleContacts} filter={filter} onChange={this.handleFilterContacts} onClick={this.handleDeleteContact}></Contacts>
         </Box>
       </Box>
     );
--- src/components/phonebook/PhoneBook.jsx
+++ src/components/phonebook/PhoneBook.jsx
@@ -1,7 +1,11 @@
 import { Box } from "components/box/Box"
 import styled from "styled-components"
 
-const NameTitle = styled.h3`
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   text-align: center;
   border-radius: ${p=>p.theme.radii.normal};
   border: ${p=>p.theme.borders.none};
@@ -11,7 +15,7 @@ const NameTitle = styled.h3`
   margin-top: ${p=>p.theme.space[3]}px; 
   font-weight: ${p=>p.theme.fontWeights.body};
   font-family: ${p=>p.theme.fonts.heading};
-  font-size: ${p=>p.theme.fontSizes[3]}px;
+  font-size: ${p=>p.theme.fontSizes[5]}px;
 `
 
 const NameInput = styled.input`
@@ -19,7 +23,16 @@ const NameInput = styled.input`
   border-radius: ${p=>p.theme.radii.normal};
   border: ${p=>p.theme.borders.none};
   padding: ${p=>p.theme.space[4]}px;
-  margin-top: ${p=>p.theme.space[3]}px; 
+  font-weight: ${p=>p.theme.fontWeights.body};
+  font-family: ${p=>p.theme.fonts.body};
+  font-size: ${p=>p.theme.fontSizes[4]}px;
+`
+
+const TelInput = styled.input`
+  text-align: center;
+  border-radius: ${p=>p.theme.radii.normal};
+  border: ${p=>p.theme.borders.none};
+  padding: ${p=>p.theme.space[4]}px;
   font-weight: ${p=>p.theme.fontWeights.body};
   font-family: ${p=>p.theme.fonts.body};
   font-size: ${p=>p.theme.fontSizes[4]}px;
@@ -46,19 +59,32 @@ const AddContactBtn = styled.button`
   }
 `
 
-export const PhoneBook = () => {
-    return <Box display= "flex" flexDirection="column" justifyContent= "space-evenly" alignItems= "start" mt="3" as={"form"}>
-        <Box>
-            <NameTitle>Name</NameTitle>
-        </Box>
+export const PhoneBook = ({name, number, onChange, onSubmit}) => {
+  return <Box onSubmit={onSubmit} display="flex" flexWrap="wrap" width="650px" justifyContent="space-evenly" alignItems="start" mt="3" as={"form"}>
+        <Label>Name
         <NameInput
             placeholder="pls input your name..."
             type="text"
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required>
+            required
+            value={name}
+            onChange={onChange}>
         </NameInput>
-        <AddContactBtn>Add contact</AddContactBtn>
+        </Label>
+        <Label>Number
+        <TelInput
+            placeholder="pls input your number..."
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={onChange}>
+        </TelInput>
+        </Label>
+        <AddContactBtn type="submit">Add contact</AddContactBtn>
     </Box>
 }
